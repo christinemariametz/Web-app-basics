3505af8918cced53e971314de40a1c295dd12e25
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-04 21:48:29 +0200

    Counter-Kata hinzugefügt

---
+++ 03-counter/script.js
@@ -0,0 +1,20 @@
+/*
+                        Counter
+Implement a web app that will count like this example. (see bootcamp side)
+
+Requirments
+
+ - Add a larger counter number
+ - The number is increased every time you
+ - click the main area of the website
+ - press the enter or space key
+ - Add a reset button that will reset the number to 0
+ - Change the color of the main area behind your counter step by  step everytime the counter is increased
+ - 100 counting steps will turn the background fully colorful
+ - After every 100 counting steps reset the background (see video)
+
+
+ 
+*/
+
+
