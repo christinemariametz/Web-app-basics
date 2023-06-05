3bd6d81ff48345ebe0831bff9c926ccce0948243
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-05 22:48:16 +0200

    Counter beendet, neue Katas hinzugefügt&started

--- 03-counter/script.js
+++ 03-counter/script.js
@@ -1,27 +1,17 @@
+
+"use strict"
+
 /*
-                        Counter
-Implement a web app that will count like this example. (see bootcamp side)
-
-Requirments
-
- - Add a larger counter number
- - The number is increased every time you
- - click the main area of the website
- - press the enter or space key
- - Add a reset button that will reset the number to 0
- - Change the color of the main area behind your counter step by  step everytime the counter is increased
- - 100 counting steps will turn the background fully colorful
- - After every 100 counting steps reset the background (see video)
-
 Gedanken: 
-- Wenn auf den body ge"click"t wird --> "counter" zählt hoch && "Prozent-Farb-Ticker" baut sich auf (von 1%-100%).
+- Wenn auf den body ge"click"t wird (Oder "enter" oder "space" - KEY gedrückt wird) --> "counter" zählt hoch && "Prozent-Farb-Ticker" baut sich auf (von 1% bis 100%).
 Zusätzlich:
 Bei "counter" === 100? dann fängt der "Prozent-Farb-Ticker" wieder bei 0 an, aber der "counter" zählt weiter (101..).
 
 Zusätzlich:
 - Wenn ich auf den Button "reset" klicke --> "counter" springt auf 0 zurück.
- 
-*/
+ */
+
+
 
 const mainCounter = document.querySelector("#counter");
 const main = document.querySelector("main");
@@ -29,19 +19,25 @@ let curValue = 0;
 let ColorValue = 0;
 const btnReset = document.querySelector("button");
 
-
-main.addEventListener("click", () => {
-curValue++;
-mainCounter.textContent = curValue;
-ColorValue++;
-if(ColorValue === 101) {
-    ColorValue = 0;
+function IncrementCounter() {
+    curValue++;
+    mainCounter.textContent = curValue;
+    ColorValue++;
+    //main.style = "--counter: " + ColorValue + "%";
+    main.style.backgroundSize = ColorValue + "% 100%";
+    if(ColorValue % 100 === 0) {ColorValue = 0;}
 }
 
-ColorValue++;
-})
+main.addEventListener("click", IncrementCounter)
 
 btnReset.addEventListener("click", () => {
     curValue = 0;
     mainCounter.textContent = curValue;
-    })
+    ColorValue = 0;
+})
+
+document.addEventListener("keydown", (event) => {
+    if(event.key === "Enter" || "Space") {
+        IncrementCounter();
+    }
+})
