bbcc2638a97d23a14abfc177a42fe24c14ed6caa
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-09 10:59:15 +0200

    Todo App weiterentwickelt

--- 00-TO-DO-APP/script.js
+++ 00-TO-DO-APP/script.js
@@ -2,6 +2,35 @@ const state = {
     todos: [
         { description: "Lern JavaScript", done: false},
         { description: "Lern CSS", done: false},
-        
+        { description: "Lern HTML", done: false},
+        { description: "Practise with CodeWarsKatas", done: false},
     ]
 }
+
+function renderTodos() {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+
+    state.todos.forEach(todo => {
+        const todoLi = document.createElement("li");
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = todo.done;
+
+        checkbox.addEventListener("checkbox", (e) => {
+            const newTodoDoneState = e.target.checked;
+            todo.done = newTodoDoneState;
+        });
+
+        todoLi.appendChild(checkbox);
+
+        const todoText = document.createTextNode(todo.description);
+        todoLi.append(todoText);
+
+        list.appendChild(todoLi);
+    });
+}
+
+renderTodos();
+
