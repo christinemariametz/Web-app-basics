b4217ca83bb77799c96e8d903fe343a83faa7845
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-11 23:21:03 +0200

    AddToDoBtn, Checkbox-Line-through hinzugefügt

--- 00-TO-DO-APP/script.js
+++ 00-TO-DO-APP/script.js
@@ -3,7 +3,7 @@
 const newToDo = document.getElementById("newToDoInput")
 const addToDoBtn = document.getElementById("add-todo");
 
-const remove = document.getElementById("remove");
+const removeBtn = document.getElementById("remove");
 
 const radioInputAll = document.getElementById("radioInputAll");
 const radioInputDone = document.getElementById("radioInputDone");
@@ -12,14 +12,11 @@ const radioInputOpen = document.getElementById("radioInputOpen");
 
 // Mock-up für newToDos:
 const state = {
-    todos: [
-        { id: 1, description: "Lern JavaScript", done: false},
-        { id: 2, description: "Lern CSS", done: false},
-        { id: 3, description: "Lern HTML", done: false},
-        { id: 4, description: "Practise with CodeWarsKatas", done: false},
-    ]
+    "1686512042647": { description: "Lern JavaScript", done: false },
+    "1686512133550": { description: "Lern CSS", done: false },
+    "1686512133551": { description: "Lern HTML", done: false },
+    "1686512133552": { description: "Practise with CodeWarsKatas", done: false }
 }
-
 
 
 // Visualisieren meiner Todos/ "Rendern"
@@ -28,7 +25,8 @@ function renderTodos() {
     const list = document.getElementById("list");
     list.innerHTML = "";
 
-    state.todos.forEach(todo => {
+    Object.keys(state).forEach(id => {
+     const todo = state[id];   
         // todoListe bauen
         const newLi = document.createElement("li");
 
@@ -37,34 +35,48 @@ function renderTodos() {
         checkbox.type = "checkbox";
         checkbox.checked = todo.done;
 
-        // Wenn in der App die checkbox geklickt wird, muss auch mein state aktualisiert sein. 
-        checkbox.addEventListener("change", (event) => {
-            const newTodoDoneState = event.target.checked;
-            todo.done = newTodoDoneState;
-        });
-
+        // description der Todos bauen und hinzufügen
+        const todoText = document.createElement("lable");
+        todoText.innerHTML = todo.description;
         // Checkbox hinzufügen
         newLi.appendChild(checkbox);
 
-
-        // description der Todos bauen und hinzufügen
-        const todoText = document.createTextNode(todo.description);
+        // Text hinzufügen
         newLi.append(todoText);
 
         // todoList hinzufügen
         list.appendChild(newLi);
+
+        // Wenn in der App die checkbox geklickt wird, muss auch mein state aktualisiert sein. 
+        checkbox.addEventListener("change", (event) => {
+            console.log(event.target.checked);
+            if(event.target.checked) {
+                todoText.style.textDecoration = "line-through";
+                todo.done = true;
+            } else {
+                todoText.style.textDecoration = "none";
+                todo.done = false;
+            }
+        });
     });
 }
 // Funktion aufrufen um den State visuell darzustellen/ zu sehen
 renderTodos();
 
 
-
-
 // Welche Events brauche ich?? 
 
     // 1. Click Event bauen für "ADD todo"
 addToDoBtn.addEventListener("click", () => {
+    //.trim um Leerzeichen zu löschen
+   const newDescription = newToDo.value.trim();
+   // Wenn....
+    if(newDescription.length < 4) {
+        return;
+    }
+   const newId = new Date().getTime();
+   state[newId] = {description:newDescription, done:false};
+
     renderTodos();
 })
 
