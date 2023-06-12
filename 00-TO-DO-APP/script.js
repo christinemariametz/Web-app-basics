e55b061594a973832d156d3a81a34eb770993df3
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-12 23:25:52 +0200

    ToDo App finalisiert

--- 00-TO-DO-APP/script.js
+++ 00-TO-DO-APP/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Welche Elemente müssen interaktiv sein?
 // ---> Variablen dafür bauen!
 const newToDoInput = document.getElementById("newToDoInput")
@@ -6,48 +8,72 @@ const removeBtn = document.getElementById("remove");
 const radioInputAll = document.getElementById("radioInputAll");
 const radioInputDone = document.getElementById("radioInputDone");
 const radioInputOpen = document.getElementById("radioInputOpen");
-
+// am Anfang haben wir ein leeres Objekt.
+let state = {};
+// ### Local Storage
+// sichern
+function safeStateToLocalStorage() {
+    localStorage.setItem("state", JSON.stringify(state));
+}
+// ### Local Storage
+// auslesen
+function readStateFromLocalStorage() {
+    let state = {};
+    const todosFromStorage = localStorage.getItem("state");
+    if(todosFromStorage !== null) {
+        state = JSON.parse(todosFromStorage);
+   }
+   return state;
+}
+state = readStateFromLocalStorage();
 
 // ### Displaying Todos: 
-// ----> Mock-up einiger ToDos bauen:
-const state = {
-    "1686512042647": { description: "Lern JavaScript", done: false },
+// ----> Mock-up:
+/*let state = {
+    "1686512042647": { 
+        description: "Lern JavaScript",
+        done: false
+    },
     "1686512133550": { description: "Lern CSS", done: false },
     "1686512133551": { description: "Lern HTML", done: false },
-    "1686512133552": { description: "Practise with CodeWarsKatas", done: false }
-};
-
+};*/
 
 // State Management: ----> Visualisieren meiner Todos/ "Rendern"
-function renderTodos() {
+function renderTodos(filterForOpen = false, filterForDone = false) {
     // meine Liste soll anfangs immer leer sein.
     const list = document.getElementById("list");
     list.innerHTML = "";
     // forEach Todo in der Liste soll das gemacht werden:
     Object.keys(state).forEach(id => {
         // wie greife ich auf die einzelnen todos zu?
-        const todo = state[id];   
-        
+        const todo = state[id];
+        // ### Filtering Todos
+        if(filterForOpen === true && todo.done === true) {
+            return;
+        }  
+        if(filterForDone === true && todo.done === false) {
+            return;
+        }
         // todoListe bauen
         const newLi = document.createElement("li");
-
         // Checkbox bauen
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.checked = todo.done;
-
         // description der Todos bauen
         const todoText = document.createElement("lable");
-        todoText.innerHTML = todo.description;
-        
+        todoText.innerHTML = todo.description;       
         // todoList, Checkbox und Text hinzufügen
         list.appendChild(newLi);
         newLi.appendChild(checkbox);
         newLi.append(todoText);
-
+        // line-through muss hier auch schon passieren!
+        if (todo.done === true) {
+            todoText.style.textDecoration = "line-through";
+        }
+        // ### Done status of Todos
         // Checkbox klicken === true/done PLUS: der Todo Text soll durchgestrichen werden
         // Checkbox wieder klicken === false/open
-        // PLUS : Wenn in der App die checkbox geklickt wird, muss auch mein state aktualisiert sein. 
         checkbox.addEventListener("change", (event) => {
             console.log(event.target.checked);
             if(event.target.checked) {
@@ -57,6 +83,7 @@ function renderTodos() {
                 todoText.style.textDecoration = "none";
                 todo.done = false;
             }
+        safeStateToLocalStorage();
         });
     });
 }
@@ -69,17 +96,59 @@ addToDoBtn.addEventListener("click", () => {
     //Variable für ein neues Todo PLUS ".trim", um Leerzeichen zu löschen:
    const newDescription = newToDoInput.value.trim();
    // Kein leeres oder unnötiges ToDo anlegen!
-   // ---> Wenn mein ToDo Text kürzer als 4 Zeichen ist, dann return.
     if(newDescription.length < 4) {
         return;
     }
-   // ---> ansonsten erstelle das neue ToDo und erstelle auch noch eine ID dafür: 
+    // ### Duplicate Check
+    const arrayOfTodoIds = Object.keys(state);
+    const arrayOfAllDescriptions = arrayOfTodoIds.map(id => state[id].description);
+    if (arrayOfAllDescriptions.includes(newDescription)) {
+        return;
+    }
+   // ---> ansonsten erstelle das neue ToDo PLUS eigene ID:
    const newId = new Date().getTime();
    state[newId] = {description:newDescription, done:false};
+    //Inputfeld wieder leeren, nachdem ein To hinzugefügt wurde:
+   newToDoInput.value = "";
+    // in local storage safen:
+   safeStateToLocalStorage();
     // state updaten:
     renderTodos();
 })
 
 
-    // 2. Remove Btn klicken
-    // 3. Filter auswählen
+
+// ### Filtering Todos
+// Wenn ich auf Open klicke...
+radioInputOpen.addEventListener("click", () => {
+    renderTodos(true, false);
+})
+// Wenn ich auf Done klicke...
+radioInputDone.addEventListener("click", () => {
+    renderTodos(false, true);
+})
+// Wenn ich auf All klicke...
+radioInputAll.addEventListener("click", () => {
+    renderTodos(false, false);
+})
+
+
+// ### Remove done Todos
+removeBtn.addEventListener("click", () => {
+    let newState = {};
+
+    Object.keys(state).forEach((id) => {
+        const todo = state[id];   
+
+        if (todo.done === false) {
+            newState[id] = todo;
+        }
+    });
+
+    state = newState;
+
+    // in local storage safen:
+    safeStateToLocalStorage();
+    // state updaten:
+    renderTodos();
+})
